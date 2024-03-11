ad475a5b69bb776e804744a56ab5f1344a24541a
mohammedjafferali <jaffercharliebe@gmail.com>
2024-03-11 18:14:59 +0530

    Adding Inheritance day 1 notes

---
+++ src/oops/inhertanceconcept/SingleInheritance.java
@@ -0,0 +1,23 @@
+package oops.inhertanceconcept;
+
+/* Rules to use the inheritance
+* 1. Child class can inherit the properties of parent class (variables and methods)
+* 2. Create object for the child class,in order to call both parent and child class stuff
+* 3. Types of inheritance - Single, Multi-level, Hierarchy and Multiple (not possible using class, but with the interface
+* 4. Single - one parent, one child
+* 5. Multi-level - GrandParent, Parent and Child
+* 6. Hierarchy - One pareny multiple childen
+* 7. Multiple - one childern will have properties of two parent classes
+*
+* */
+public class SingleInheritance { //There should be one only public class allowed in one class
+    
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
