94eee545208545bde62ae581708da7f298397c90
mohammedjafferali <jaffercharliebe@gmail.com>
2024-03-11 21:40:26 +0530

    Adding Inheritance day 1 notes

--- src/oops/inhertanceconcept/SingleInheritance.java
+++ src/oops/inhertanceconcept/SingleInheritance.java
@@ -10,13 +10,40 @@ package oops.inhertanceconcept;
 * 7. Multiple - one childern will have properties of two parent classes
 *
 * */
+
+class Parent{
+
+    int age;
+    String name;
+    String surname;
+    void eatingWithSpoon()
+    {
+        System.out.println(" I eat food with spoon ");
+    }
+
+}
+class Child extends Parent{
+    void favoriteSubject(String subject)
+    {
+        System.out.print(subject);
+    }
+}
+
 public class SingleInheritance { //There should be one only public class allowed in one class
-    
-
-
-
-
     public static void main(String[] args) {
+
+    //Create an object for the child class so extended class properties can be used in the main
+        Child child1 = new Child();
+        child1.age = 12;
+        Parent parent = new Parent();
+        parent.age = 36;
+        child1.name = "Anil Kumar";
+        parent.name = "Mukesh Kumar";
+        child1.surname = parent.name;
+
+        System.out.println("I am "+ child1.name + " son of "+ parent.name + " And I am "+ child1.age + " years old.");
+        System.out.print("My favourite subject is ");
+        child1.favoriteSubject("Maths");
 
     }
 
