33c0c5c756534e70b1aa63128c94ad3962ac4d8a
“Mohammed <mohammedjafferali.abubakkarsiddiq@bt.com>
2025-06-20 19:27:34 +0530

    Optimised folder structure

--- src/DataStructure/collections/HashSet.java
+++ src/DataStructure/collections/HashSet.java
@@ -15,7 +15,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class HashSetCollection {
+public class HashSet {
 
 
     public static void main(String[] args) {
